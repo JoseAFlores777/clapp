9c0309519c78039c9db71a7dba0fcd5795cbfb62
José Adolfo Izaguirre <jizaguirre@applaudostudios.dev>
2022-07-05 22:24:20 -0600

    add lib structure

--- projects/clapp/src/lib/clapp.module.ts
+++ projects/clapp/src/lib/clapp.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ClappComponent } from './clapp.component';
+import { IconButtonComponent } from './components/buttons/icon-button/icon-button.component';
+import { SidebarComponent } from './patterns/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
-    ClappComponent
+    ClappComponent,
+    IconButtonComponent,
+    SidebarComponent
   ],
   imports: [
   ],
