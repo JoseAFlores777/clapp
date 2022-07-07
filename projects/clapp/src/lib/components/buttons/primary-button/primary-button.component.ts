b1b379df369ecdfd662b0216651e2a80dd3f37c5
DavidBoccalandro <davidboccalandro@hotmail.com>
2022-07-06 23:40:32 -0300

    add logic approach primary-button

--- projects/clapp/src/lib/components/buttons/primary-button/primary-button.component.ts
+++ projects/clapp/src/lib/components/buttons/primary-button/primary-button.component.ts
@@ -1,19 +1,20 @@
-import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'cl-primary-button',
   templateUrl: './primary-button.component.html',
   styleUrls: ['./primary-button.component.scss'],
 })
-export class PrimaryButtonComponent implements AfterViewInit {
-  disabled: boolean = false;
-  @ViewChild('someInput') someInput!: ElementRef<HTMLElement>;
+export class PrimaryButtonComponent implements OnInit {
+  @Input('disabled') disabled = false;
+  @Input('text') text = 'Primary';
+  // @ViewChild('primaryButton') someInput!: ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  ngAfterViewInit(): void {
-    console.log(this.someInput);
-  }
+  // ngAfterViewInit(): void {
+  //   this.someInput.nativeElement.disabled = this.disabled;
+  // }
 }
